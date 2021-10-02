2c735dfee946d0463dbd8d406d204e3fbc13e397
sunny <dmstjs969@naver.com>
2021-10-02 22:13:51 +0900

    쇼핑몰

--- src/main/java/com/shop/entity/Board.java
+++ src/main/java/com/shop/entity/Board.java
@@ -1,4 +1,32 @@
 package com.shop.entity;
 
-public class Board {
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "board")
+@Getter
+@Setter
+@ToString
+public class Board extends BaseEntity{
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String title;
+
+  private String content;
+
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name="member_id")
+  private Member member;
+
+
+
 }
